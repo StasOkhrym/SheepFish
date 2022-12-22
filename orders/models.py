a5a647e2f5e741825b46bb6d34dd557156a4a2f4
Stas Okhrym <s.okhrym@gmail.com>
2022-12-22 11:35:15 +0200

    Making templates

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,5 @@
+from decimal import Decimal
+
 from django.db import models
 
 
@@ -5,6 +7,10 @@ class Item(models.Model):
     name = models.CharField(max_length=64)
     quantity = models.IntegerField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
+
+    @property
+    def item_price(self) -> Decimal:
+        return self.price * self.quantity
 
     def __str__(self) -> str:
         return f"{self.name}: (quantity:{self.quantity}, price: {self.price})"
@@ -16,3 +22,7 @@ class Order(models.Model):
 
     def __str__(self) -> str:
         return f"Order: {self.order_number}"
+
+    @property
+    def total_price(self) -> Decimal:
+        return sum([item.item_price for item in self.items.objects.all()])
